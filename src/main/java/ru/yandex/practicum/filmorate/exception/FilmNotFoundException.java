ad68a8e7110e824bf0cae5fd4463d7b6fd4db97d
SergeyPrisyazhnyuk <vasilisk811@yandex.ru>
2023-08-21 15:12:11 +0300

    feat : Sprint 9 Iter 12 added controllers for films and users

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
@@ -2,9 +2,6 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class FilmNotFoundException extends RuntimeException{
 
-    public FilmNotFoundException() {
-    }
-
     public FilmNotFoundException(String message) {
         super(message);
     }

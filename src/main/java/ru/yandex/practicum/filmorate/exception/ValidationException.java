25256265a6b359d53830c4402d769fe65044fe3e
SergeyPrisyazhnyuk <130796016+SergeyPrisyazhnyuk@users.noreply.github.com>
2023-09-13 00:22:49 +0300

    feat : added likes and friends logic

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.exception;
 import lombok.Getter;
 
 @Getter
-public class ValidationException extends Exception{
+public class ValidationException extends Exception {
     public ValidationException(String message) {
         super(message);
     }
